9454f32b95a8da8e4d1f8ae2cae9c84331e5d5e7
01000001kuma <a_illera@yahoo.com>
2022-04-11 18:43:19 +0200

    save: Full leson

--- 04-class-js_loops.js
+++ 04-class-js_loops.js
@@ -1,1 +1,16 @@
 // Class code examples
+
+const text = 'Fluffy'
+
+for (let i=0; i < text.length; i++) {
+    console.log(text.charAt(i));
+}
+
+let inputValue = 9;
+console.log("Input a value from 10 to 100");
+
+while (inputValue) {
+    value = console.log("Input a value grater than 10...")    
+}
+
+console.log('Finally , dude!')
